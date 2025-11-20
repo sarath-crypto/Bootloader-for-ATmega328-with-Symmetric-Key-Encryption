2b6a604498eff1a6232211b0b652bf3d2ff18e60
sarath-crypto <76784482+sarath-crypto@users.noreply.github.com>
2025-11-20 18:45:47 +0530

    Refactor srec.sh for bootloader compilation and flashing
    Clean up previous build files, compile bootloader, and program the microcontroller with the new firmware.

--- srec.sh
+++ srec.sh
@@ -1,6 +1,25 @@
+rm  *.elf *.hex *.o
+rm tags
+ctags -R *
+
+avr-g++ -g -Os -mmcu=atmega328p -c bootloader.cpp 
+avr-g++ -g -mmcu=atmega328p -o bootloader.elf  bootloader.o
+avr-g++ -g -Wall -Os -mmcu=atmega328 -Wl,-Ttext=0x7000 -o bootloader.elf bootloader.o 
+avr-objcopy -j .text -j .data -O ihex bootloader.elf bootloader.hex
+nb=$(srec_info bootloader.hex -intel)
+nb=$(echo $nb | cut -d ' ' -f 12-)
+nb=$((16#$nb))
+nb=$((nb-28672))
+nb=$((nb+1))
+echo $nb"/2048 words" 
+
+avrdude -c usbasp -p m328p -B5
+avrdude -c usbasp -p m328p -U hfuse:w:0xD8:m
+avrdude -c usbasp -p m328p -U lfuse:w:0xE2:m
+avrdude -c usbasp -p m328p -U efuse:w:0xFF:m
+avrdude -c usbasp -p m328p -U flash:w:bootloader.hex
+
 srec_cat bootloader.hex -intel -exclude 0x0000 0x3800 -o bootloader.hex -intel
-srec_cat app.hex -intel bootloader.hex -intel -offset -minimum-addr app.hex -intel -o firmw.hex -intel
-./encryptor -r firmw.hex efirmw.hex <key>
 
 line=$(tail --lines=1 keygen.cfg)
 w=( $line )
@@ -22,3 +41,6 @@ srec_info sige.hex -intel
 srec_cat firmw.hex -intel sige.hex -intel -offset -minimum-addr firmw.hex -intel -o fw.hex -intel
 srec_info fw.hex -intel
 rm sige.hex -f
+
+srec_cat app.hex -intel bootloader.hex -intel -offset -minimum-addr app.hex -intel -o firmw.hex -intel
+./encryptor -r firmw.hex efirmw.hex <key>
